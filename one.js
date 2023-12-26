a78eaf5c282638eec5b77aa51d3863fd9adaab6d
syabdull <syab5050@gmail.com>
2023-12-26 12:47:34 +0530

    function inside function

--- one.js
+++ one.js
@@ -1,9 +1,17 @@
-function fun(a, b) {
-  console.log(a);
-  b();
-}
-function myFun() {
-  console.log("iam also javeed");
-}
+// function fun(a, b) {
+//   console.log(a);
+//   b();
+// }
+// function myFun() {
+//   console.log("iam also javeed");
+// }
 
-fun("javeed", myFun);
+// fun("javeed", myFun);
+
+function fun() {
+  function myFun() {
+    console.log("Hello Animal");
+  }
+  myFun();
+}
+fun();
